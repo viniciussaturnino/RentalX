4f6a535f2cf992070a955697620618b9dcedcd0f
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-03-26 10:55:21 -0300

    Create category model and repository

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.status(200).json(all);
+});
+
+export { categoriesRoutes };
