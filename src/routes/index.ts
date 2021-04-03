9ed7d7dab7d49285f8092ca8b60c289d12953a26
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-04-03 20:08:28 -0300

    feat: create users routes with post method

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
+import { userRoutes } from './users.routes';
 
 const router = Router();
 
+router.use('/users', userRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 
