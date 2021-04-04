81df9618e0072b6fa71777b044378bc0d49d4e54
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-04-03 21:36:00 -0300

    feat: generate token and made login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { userRoutes } from './users.routes';
@@ -9,5 +10,6 @@ const router = Router();
 router.use('/users', userRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
+router.use(authenticateRoutes);
 
 export { router };
