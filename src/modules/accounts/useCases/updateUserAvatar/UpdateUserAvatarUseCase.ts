6c3200ca3751b7735c5d65f92bb965190fecf04a
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-04-10 13:10:49 -0300

    refactor: change imports in paths

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
-import { deleteFile } from '../../../../utils/file';
-import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+import { UsersRepository } from '@modules/accounts/repositories/implementations/UsersRepository';
+import { deleteFile } from '@utils/file';
 
 interface IRequest {
   user_id: string;
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 
-import { AppError } from './errors/AppError';
+import { AppError } from '@errors/AppError';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
 import './database';
-import './shared/container';
+import '@shared/container';
 
 const app = express();
 
