7b803f301102997075f35ba3de59e2a4827168c6
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-04-16 22:04:30 -0300

    feat: create entity car structure in typeorm

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { authenticateRoutes } from './authenticate.routes';
+import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { userRoutes } from './users.routes';
@@ -8,6 +9,7 @@ import { userRoutes } from './users.routes';
 const router = Router();
 
 router.use('/users', userRoutes);
+router.use('/cars', carsRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use(authenticateRoutes);
