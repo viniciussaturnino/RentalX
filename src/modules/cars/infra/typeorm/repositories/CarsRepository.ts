44d1e7e0458cc51faafd8942ab3f492413d43a8e
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-04-19 19:27:50 -0300

    feat: create new function for list all cars and filter them

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -42,6 +42,31 @@ class CarsRepository implements ICarsRepository {
 
     return car;
   }
+
+  async findAllAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder('c')
+      .where('available = :available', { available: true });
+
+    if (brand) {
+      carsQuery.andWhere('brand = :brand', { brand });
+    }
+
+    if (name) {
+      carsQuery.andWhere('name = :name', { name });
+    }
+
+    if (category_id) {
+      carsQuery.andWhere('category_id = :category_id', { category_id });
+    }
+
+    const cars = await carsQuery.getMany();
+    return cars;
+  }
 }
 
 export { CarsRepository };
