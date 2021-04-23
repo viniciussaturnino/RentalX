2951881dcbd06c71145381e8da08baa848ebf358
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-04-22 21:30:54 -0300

    refactor: change server structure

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,37 +1,3 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import swaggerUI from 'swagger-ui-express';
-
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
-
-import swaggerFile from '../../../swagger.json';
-import { router } from './routes';
-
-import '@shared/container';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
-
-app.use(router);
-
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internat server error - ${err.message}`,
-    });
-  }
-);
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server is running!'));
