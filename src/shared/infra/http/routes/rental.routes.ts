f38df671803aad5d69d9997bc815a178d3c719d2
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-04-24 16:37:02 -0300

    feat: create car devolution feature

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 const rentalRoutes = Router();
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post(
+  '/devolution/:id',
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export { rentalRoutes };
