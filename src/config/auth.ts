a3893ba37369c6fddcde5d0ad969fdb83d953fa4
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-05-01 16:16:35 -0300

    feat: create repository and generate refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'fe46f2a4278997302f9feb069f2f4c68',
+  expires_in_token: '15m',
+  secret_refresh_token: '6b305afc7d6983860f2eb7b1c2a37730',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
