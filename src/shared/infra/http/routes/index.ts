85ec2155bc34e1552e74a26275faadbef23a44cc
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-05-04 12:19:40 -0300

    feat: implement send mail feature

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { passwordRoutes } from './password.routes';
 import { rentalRoutes } from './rental.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { userRoutes } from './users.routes';
@@ -14,6 +15,7 @@ router.use('/cars', carsRoutes);
 router.use('/rentals', rentalRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
+router.use('/password', passwordRoutes);
 router.use(authenticateRoutes);
 
 export { router };
