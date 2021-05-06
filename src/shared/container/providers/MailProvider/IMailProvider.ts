f70cf31d9d3e2525e3422bf160cc830c1321079e
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-05-06 19:53:43 -0300

    feat: create template email with handlebars

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 interface IMailProvider {
-  sendMail(to: string, subject: string, body: string): Promise<void>;
+  sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void>;
 }
 
 export { IMailProvider };
--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import fs from 'fs';
+import handleBars from 'handlebars';
 import nodemailer, { Transporter } from 'nodemailer';
 import { injectable } from 'tsyringe';
 
@@ -25,13 +29,23 @@ class EtherealMailProvider implements IMailProvider {
       .catch((err) => console.log(err));
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+
+    const templateParse = handleBars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: 'Rentx <noreply@rentx.com.br>',
       subject,
-      text: body,
-      html: body,
+      html: templateHTML,
     });
 
     console.log('Message sent: %s', message.messageId);
